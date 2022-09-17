cc22e0ac896e447242d3b68386e52b4555ae003f
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-09-17 04:51:40 -0700

    implement common select option component

---
+++ src/components/common/Select.jsx
@@ -0,0 +1,27 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+export default function SelectField({ id, name, value, options, onChange }) {
+  const capitalizeFirstLetter = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
+  return (
+    <FormControl fullWidth sx={{ marginBottom: "24px" }}>
+      <InputLabel id="">{name}</InputLabel>
+      <Select
+        key={id + "-select"}
+        labelId={id + "-label-select"}
+        id="demo-simple-select"
+        value={value}
+        label={capitalizeFirstLetter(id)}
+        onChange={onChange}
+      >
+        {options?.map(({ value, name }, index) => {
+          return (
+            <MenuItem key={index} value={value} sx={{ textAlign: "left" }}>
+              {capitalizeFirstLetter(name)}
+            </MenuItem>
+          );
+        })}
+      </Select>
+    </FormControl>
+  );
+}
