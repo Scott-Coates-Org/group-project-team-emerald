b13d25989af9b09cd4b028fa54665d6348cdd384
lone-max <59314817+lone-max@users.noreply.github.com>
2022-09-19 14:18:31 +0300

    Pages

---
+++ src/pages/customer/homepage/homepage.jsx
@@ -0,0 +1,16 @@
+import { Button, Card, CardContent } from "@mui/material";
+import styles from "./homepage.module.css";
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div className={styles.container}>
+      <Card sx={{ minWidth: 275, minHeight: 275 }}>
+        <CardContent>Card Content</CardContent>
+      </Card>
+      <Button variant="contained" size="large">
+        Buy Tickets
+      </Button>
+    </div>
+  );
+}
