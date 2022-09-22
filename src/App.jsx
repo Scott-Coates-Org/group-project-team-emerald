9d3ccaf245c585c8e456cd538f5a5a93e2c3b009
Abdul Rehman <adeel.cs@gmail.com>
2022-09-22 17:06:12 +0500

    Added lazy loading on page route/components to increase page load & performace

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,80 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Products from './pages/product/product';
-import Login from './pages/login/Login';
-import AdminHome from './pages/admin/index';
-import Homepage from './pages/customer/homepage/homepage';
-import Checkout from './pages/customer/checkout/checkout';
-import AdminLayout from './components/adminLayout';
-import UserLayout from './components/userLayout';
-import AddRoom from './components/AddRoom/AddRoom';
-import AddProduct from './components/AddProduct/AddProduct';
 import { useSelector } from 'react-redux';
 
+import AdminLayout from './components/adminLayout';
+import UserLayout from './components/userLayout';
+
+const Products = lazy(() => import('./pages/product/product'));
+const Login = lazy(() => import('./pages/login/Login'));
+const AdminHome = lazy(() => import('./pages/admin/index'));
+const Homepage = lazy(() => import('./pages/customer/homepage/homepage'));
+const Checkout = lazy(() => import('./pages/customer/checkout/checkout'));
+const AddRoom = lazy(() => import('./components/AddRoom/AddRoom'));
+const AddProduct = lazy(() => import('./components/AddProduct/AddProduct'));
+
 function App() {
-  const currentUser = useSelector((state) => state.auth.user);
+  const currentUser = useSelector(state => state.auth.user);
 
   const RequireAuth = ({ children }) => {
     return currentUser ? children : <Navigate to="/admin/login" />;
   };
 
   return (
-    <Routes>
-      {/* Admin routes */}
-      <Route path="/admin/login" element={<Login />} />
-      <Route element={<AdminLayout />}>
-        <Route
-          path="/admin"
-          element={
-            <RequireAuth>
-              <AdminHome />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/products"
-          element={
-            <RequireAuth>
-              <Products />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/addProducts"
-          element={
-            <RequireAuth>
-              <AddProduct />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/rooms/add"
-          element={
-            <RequireAuth>
-              <AddRoom />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/admin/rooms"
-          element={
-            <RequireAuth>
-              <Products />
-            </RequireAuth>
-          }
-        />
-      </Route>
+    <Suspense fallback={<h1>Loading..</h1>}>
+      <Routes>
+        {/* Admin routes */}
+        <Route path="/admin/login" element={<Login />} />
+        <Route element={<AdminLayout />}>
+          <Route
+            path="/admin"
+            element={
+              <RequireAuth>
+                <AdminHome />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/admin/products"
+            element={
+              <RequireAuth>
+                <Products />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/admin/addProducts"
+            element={
+              <RequireAuth>
+                <AddProduct />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/admin/rooms/add"
+            element={
+              <RequireAuth>
+                <AddRoom />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/admin/rooms"
+            element={
+              <RequireAuth>
+                <Products />
+              </RequireAuth>
+            }
+          />
+        </Route>
 
-      {/* Customer Routes */}
-      <Route element={<UserLayout />}>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+        {/* Customer Routes */}
+        <Route element={<UserLayout />}>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
