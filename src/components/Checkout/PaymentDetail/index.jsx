948d89908a009f3d4d278bd229ed7c7aa003e3b0
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-10-07 14:40:51 -0700

    update function url on client and server side

--- src/components/Checkout/PaymentDetail/index.jsx
+++ src/components/Checkout/PaymentDetail/index.jsx
@@ -10,14 +10,16 @@ import { Box } from '@mui/material';
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripePromise = loadStripe('{{CLIENT KEY}}');
+const stripePromise = loadStripe(
+  'pk_test_51JJJyJPnV8MRFpwOkRHMOPch9BmKqeU5gtHwyXIrmd8f40w6lXPsPhscIBdrBlYU5fdz3mk8GKfSwP4STOp0RziO00nff73WAP'
+);
 
 export default function PaymentDetail() {
   const [clientSecret, setClientSecret] = useState('');
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch('http://localhost:4242/create-payment-intent', {
+    fetch('http://localhost:5001/team-emerald/us-central1/createPayment', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ items: [{ id: 'xl-tshirt' }] }),
@@ -25,7 +27,8 @@ export default function PaymentDetail() {
       .then(res => res.json())
       .then(data => {
         setClientSecret(data.clientSecret);
-      });
+      })
+      .catch(error => console.log(error));
   }, []);
 
   const appearance = {
