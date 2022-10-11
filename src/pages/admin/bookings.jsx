833eec057ad6afe52bdedc952958a0f8ce8dd1ea
Ian-Mark <ianarapkirui@gmail.com>
2022-10-11 13:08:24 +0300

    bookings view: fetch data from firestore

--- src/pages/admin/bookings.jsx
+++ src/pages/admin/bookings.jsx
@@ -1,12 +1,26 @@
 import FeaturedInfo from 'components/bookings/featuredInfo';
 import Info from 'components/bookings/info';
 
-export default function Bookings({ bookings }) {
+import { useEffect, useState } from 'react';
+import { getCollection } from 'utils/firebase';
+
+export default function Bookings() {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const bookings = await getCollection('bookings');
+      setRows(bookings);
+    }
+
+    fetchData();
+  }, []);
+  if (!rows.length) return <div>Loading...</div>;
   return (
     <div>
       <h2>Bookings</h2>
       <FeaturedInfo />
-      <Info bookings={bookings} />
+      <Info bookings={rows} />
     </div>
   );
 }
