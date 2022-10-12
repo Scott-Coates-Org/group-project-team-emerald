d7551eec0f4317d8ff9e44fd2e22d917872d74c8
Abdul Rehman <adeel.cs@gmail.com>
2022-10-12 13:30:08 +0500

    task 15 UI finalized

--- src/components/Checkout/Product/prodDesc.jsx
+++ src/components/Checkout/Product/prodDesc.jsx
@@ -1,8 +1,8 @@
 import styles from './styling/prod.module.css';
-import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import { Typography, Button, ButtonGroup } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+
 export default function ProdDesc() {
   const times = [
     '09:00 AM',
@@ -23,29 +23,32 @@ export default function ProdDesc() {
     '04:30 PM',
     '05:00 PM',
   ];
+
   return (
     <>
       <div className={styles.descContainer}>
         {times.map(item => (
-          <>
-            <Button className={styles.timeBtn} variant="contained">
-              {item}
-            </Button>
-          </>
+          <Button key={item} className={styles.timeBtn} variant="contained">
+            {item}
+          </Button>
         ))}
       </div>
+
       <div className={styles.bottom}>
         <Typography>60 minutes</Typography>
         <Typography>$30</Typography>
-        <div className={styles.counter}>
-          <div className={styles.iconCont}>
-            <AddIcon className={styles.icon} />
-          </div>
-          <span className={styles.count}>3</span>
-          <div className={styles.iconCont1}>
+
+        <ButtonGroup variant="outlined" aria-label="outlined button group">
+          <Button>
             <RemoveIcon className={styles.icon} />
-          </div>
-        </div>
+          </Button>
+          <Button disableRipple className={styles.count}>
+            3
+          </Button>
+          <Button>
+            <AddIcon className={styles.icon} />
+          </Button>
+        </ButtonGroup>
       </div>
     </>
   );
--- src/components/Checkout/Product/prodSelector.jsx
+++ src/components/Checkout/Product/prodSelector.jsx
@@ -1,9 +1,12 @@
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useState } from 'react';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ProdDesc from './prodDesc';
 import styles from './styling/prod.module.css';
 
 export default function ProdSelector() {
@@ -12,18 +15,17 @@ export default function ProdSelector() {
   const handleChange = panel => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
+
   return (
     <Accordion
       expanded={expanded === 'panel1'}
       elevation={0}
       onChange={handleChange('panel1')}
-      className={styles.accordion}
     >
       <AccordionSummary
         expandIcon={<ExpandMoreIcon />}
         aria-controls="panel1bh-content"
         id="panel1bh-header"
-        className={styles.summary}
       >
         <img
           src="https://firebasestorage.googleapis.com/v0/b/team-emerald.appspot.com/o/images%2Fd09e386b-6a51-4716-918c-abd8e0351fb4?alt=media&token=eec8015d-041f-4a2d-8224-6f09f5900a27"
@@ -31,18 +33,17 @@ export default function ProdSelector() {
           className={styles.prodImg}
         />
 
-        <Typography
-          className={styles.prodName}
-          sx={{ color: 'text.secondary' }}
-        >
+        <Typography variant="bold" className={styles.prodName}>
           Product Name
         </Typography>
       </AccordionSummary>
+
       <AccordionDetails>
-        <Typography>
-          Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
-          Aliquam eget maximus est, id dignissim quam.
+        <Typography sx={{ pb: '1rem' }} component="div">
+          Product Description.
         </Typography>
+
+        <ProdDesc />
       </AccordionDetails>
     </Accordion>
   );
